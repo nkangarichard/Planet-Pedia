7b5d69bd4e2363e3ed32084808e0de5f6d81ae5f
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2023-11-15 15:02:09 -0500

    Initial Commit

---
+++ Planet Pedia/Planet_PediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Planet_PediaApp.swift
+//  Planet Pedia
+//
+//  Created by Richard Nkanga on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Planet_PediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
